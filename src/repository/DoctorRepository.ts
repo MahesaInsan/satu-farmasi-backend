4cedff6bff788e5f7c2588939e08c49774749591
kennethwn <kenthnov117@gmail.com>
2024-07-22 19:09:40 +0700

    feat: add doctor crud repository

--- src/repository/DoctorRepository.ts
+++ src/repository/DoctorRepository.ts
@@ -8,7 +8,7 @@ export default class DoctorRepository extends BaseREpository{
 
     public async emailIsExist(email: string): Promise<Boolean>{
         try{
-            const doctor = await this.Prisma.doctor.findUnique({where: {email: email}});
+            const doctor = await this.Prisma.doctor.findFirst({where: {email: email}});
             return doctor !== null;
         } catch (error) {
             console.error('Error checking email:', error);
@@ -28,25 +28,16 @@ export default class DoctorRepository extends BaseREpository{
 
     public async getDoctorByEmail(email: string): Promise<Doctor | null>{
         try {
-            return await this.Prisma.doctor.findUnique({where: {email: email}});
+            return await this.Prisma.doctor.findFirst({where: {email: email}});
         } catch (error) {
             console.error('Error getting doctor by email:', error);
             throw new Error('Failed to get doctor');
         }
     }
-}
-import { Doctor, PrismaClient } from "@prisma/client";
-
-export default class DoctorRepository {
-    private readonly prisma: PrismaClient;
-
-    constructor() {
-        this.prisma = new PrismaClient();
-    }
 
     public async getAllDoctors(): Promise<Doctor []>{
         try {
-            return await this.prisma.doctor.findMany();
+            return await this.Prisma.doctor.findMany();
         } catch (error) {
             console.error('Error getting all doctor:', error);
             throw new Error('Failed to get doctor');
@@ -55,10 +46,28 @@ export default class DoctorRepository {
 
     public async getDoctorById(id: number): Promise<Doctor | null> {
         try {
-            return await this.prisma.doctor.findUnique({where: {id: id}});
+            return await this.Prisma.doctor.findUnique({where: {id: id}});
         } catch (error) {
             console.error('Error getting doctor by id:', error);
             throw new Error('Failed to get doctor');
+        }
+    }
+
+    public async getDoctorByNik(nik: string): Promise<Doctor | null> {
+        try {
+            return await this.Prisma.doctor.findUnique({where: {nik: nik}});
+        } catch (error) {
+            console.error('Error getting doctor by nik:', error);
+            throw new Error('Failed to get doctor');
+        }
+    }
+
+    public async editDoctor(doctor: Doctor): Promise<Doctor> {
+        try {
+            return await this.Prisma.doctor.update({where: {nik: doctor.nik}, data: doctor});
+        } catch (error) {
+            console.error('Error editing doctor:', error);
+            throw new Error('Failed to edit doctor');
         }
     }
 }
