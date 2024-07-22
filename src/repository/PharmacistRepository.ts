be7524fd7e94f4706dd124548b3745d2d38cdc26
kennethwn <kenthnov117@gmail.com>
2024-07-22 19:10:20 +0700

    feat: add method for edit pharmacist and get pharmacist by nik

--- src/repository/PharmacistRepository.ts
+++ src/repository/PharmacistRepository.ts
@@ -8,7 +8,7 @@ export default class PharmacistRepository extends BaseREpository{
 
     public async emailIsExist(email: string): Promise<Boolean>{
         try{
-            const admin = await this.Prisma.pharmacist.findUnique({where: {email: email}});
+            const admin = await this.Prisma.pharmacist.findFirst({where: {email: email}});
             return admin !== null;
         } catch (error) {
             console.error('Error checking email:', error);
@@ -28,16 +28,16 @@ export default class PharmacistRepository extends BaseREpository{
 
     public async getPharmacistByEmail(email: string): Promise<Pharmacist | null>{
         try {
-            return await this.Prisma.pharmacist.findUnique({where: {email: email}});
+            return await this.Prisma.pharmacist.findFirst({where: {email: email}});
         } catch (error) {
             console.error('Error getting pharmacist by email:', error);
             throw new Error('Failed to get pharmacist');
         }
     }
-    
+
     public async getAllPharmacists(): Promise<Pharmacist []> {
         try {
-            return this.prisma.pharmacist.findMany();
+            return this.Prisma.pharmacist.findMany();
         } catch (error) {
             console.error('Error getting all pharmacist:', error);
             throw new Error('Failed to get pharmacist');
@@ -46,10 +46,28 @@ export default class PharmacistRepository extends BaseREpository{
     
     public async getPharmacistById(id: number): Promise<Pharmacist | null> {
         try {
-            return this.prisma.pharmacist.findUnique({ where: { id: id } });
+            return this.Prisma.pharmacist.findUnique({ where: { id: id } });
         } catch (error) {
             console.error('Error getting pharmacist by id:', error);
             throw new Error('Failed to get pharmacist');
+        }
+    }
+
+    public async getPharmacistByNik(nik: string): Promise<Pharmacist | null> {
+        try {
+            return this.Prisma.pharmacist.findFirst({where: {nik: nik}});
+        } catch (error) {
+            console.error('Error getting pharmacist by nik:', error);
+            throw new Error('Failed to get pharmacist');
+        }
+    }
+
+    public async editPharmacist(pharmacist: Pharmacist): Promise<Pharmacist | null> {
+        try {
+            return this.Prisma.pharmacist.update({ where: { nik: pharmacist.nik }, data: pharmacist });
+        } catch (error) {
+            console.error('Error updating pharmacist:', error);
+            throw new Error('Failed to update pharmacist');
         }
     }
 }
