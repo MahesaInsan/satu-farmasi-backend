404b84bd9701e1d2c762c73fdec41c2b53cc6c5c
Mahesa Insan <mahesainsan@gmail.com>
2024-08-10 11:07:33 +0700

    feat: added get all and filtered prescription list endpoint

--- src/repository/PrescriptionRepository.ts
+++ src/repository/PrescriptionRepository.ts
@@ -1,5 +1,6 @@
 import {Prescription, PrismaClient} from "@prisma/client";
 import IdVO from "../model/VOs/IdVO";
+import PrescriptionSummaryVO from "../model/VOs/PrescriptionSummaryVO";
 
 export default class PrescriptionRepository{
     private readonly prisma: PrismaClient;
@@ -21,14 +22,65 @@ export default class PrescriptionRepository{
         }
     }
 
-    public async getPrescriptionDetail(): Promise<Prescription[]>{
+    public async getAllPrescriptionByUsername(username: string): Promise<PrescriptionSummaryVO[]>{
         try {
             return this.prisma.prescription.findMany({
-                include: {
-                    patient: true,
-                    diagnose: true,
-                    medicineList: true
-                }
+                where: {
+                    patient: {
+                        name: {
+                            contains: username
+                        }
+                    },
+                    is_active: true
+                },
+                select: {
+                    id: true,
+                    created_at: true,
+                    patient: {
+                        select: {
+                            name: true
+                        },
+                    },
+                    status: true
+                },
+                orderBy: [
+                    {
+                        status: "asc"
+                    },
+                    {
+                        created_at: "asc"
+                    }
+                ]
+            })
+        } catch (error){
+            throw error as string
+        }
+    }
+
+    public async getAllPrescription(): Promise<PrescriptionSummaryVO[]>{
+        try {
+            return this.prisma.prescription.findMany({
+                where: {
+                  is_active: true
+                },
+                select: {
+                    id: true,
+                    created_at: true,
+                    patient: {
+                        select: {
+                            name: true
+                        },
+                    },
+                    status: true
+                },
+                orderBy: [
+                    {
+                        status: "asc"
+                    },
+                    {
+                        created_at: "asc"
+                    }
+                ]
             })
         } catch (error){
             throw error as string
