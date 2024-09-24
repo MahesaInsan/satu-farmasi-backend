b0c7207d955fc37b82f2f57d3c7d83dc5315b109
orangeMangoDImz <hakuka123osaimo456@gamil.com>
2024-09-24 20:39:28 +0700

    tech-debt(validation): add validation for generic name

--- src/model/response/BaseResponse.ts
+++ src/model/response/BaseResponse.ts
@@ -6,20 +6,22 @@ export default class BaseResponse<T> {
     public status: string | undefined;
     public message: string | undefined;
     public data: T | undefined;
+    public errors: T | undefined;
 
-    constructor(code?: number, status?: string, message?: string, data?: T) {
+    constructor(code?: number, status?: string, message?: string, data?: T, errors?: T) {
         this.code = code;
         this.status = status;
         this.message = message;
         this.data = data;
+        this.errors = errors;
     }
 
     public ok(data?: T, message?: string): BaseResponse<T> {
         return new BaseResponse<T>(200, "OK", message, data)
     }
 
-    public badRequest(message: string, data?: T | undefined): BaseResponse<T> {
-        return new BaseResponse<T>(400, "Bad Request", message, data)
+    public badRequest(message: string, errors?: T | undefined): BaseResponse<T> {
+        return new BaseResponse<T>(400, "Bad Request", message, undefined, errors)
     }
 
     public constructErrorHandler(error: object) {
