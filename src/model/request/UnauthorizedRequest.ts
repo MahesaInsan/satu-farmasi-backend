f2fab44df67bcd191e2c0515dc3fbd6505e839ea
orangeMangoDImz <hakuka123osaimo456@gamil.com>
2024-09-28 14:49:16 +0700

    config: update config for building app test

--- src/model/request/UnauthorizedRequest.ts
+++ src/model/request/UnauthorizedRequest.ts
@@ -1,7 +1,7 @@
 import BaseErrorRequest from "./BaseRequest/BaseErrorRequest";
 
 export default class Unauthorized extends BaseErrorRequest{
-    constructor(err: string, code: number, message: string) {
-        super(err, code, message)
+    constructor(err: string, code: number, message: string, errors: Array<Object>) {
+        super(err, code, message, errors)
     }
 }
