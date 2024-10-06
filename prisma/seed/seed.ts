95d4b27fbf9c66c0fc0ada6c34d1c16c8a618f93
orangeMangoDImz <hakuka123osaimo456@gamil.com>
2024-10-06 20:10:39 +0700

    chore(comment): add comment to run seeder script

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,3 +1,5 @@
+// Command: npm run seed
+
 import { createSeedClient, SeedClient } from "@snaplet/seed";
 import { faker } from '@faker-js/faker';
 import UserService from "../../src/service/UserService";
