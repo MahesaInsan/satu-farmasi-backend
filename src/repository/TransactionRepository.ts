73f8051d2a985cde1f1af2ae7962e3e3aabfda54
kennethwn <kenthnov117@gmail.com>
2024-11-19 21:06:23 +0700

    fix: fix error on get transaction by status

--- src/repository/TransactionRepository.ts
+++ src/repository/TransactionRepository.ts
@@ -192,6 +192,7 @@ export default class TransactionRepository{
                     },
                     prescription: {
                         select: {
+                            id: true,
                             status: true
                         }
                     }
